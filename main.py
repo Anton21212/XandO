7813ce735ba2a3d2adbd02dc2dbbaaa853fce52a
Admin <sortof00@mail.ru>
2022-04-25 22:56:57 +0300

    new_version

--- main.py
+++ main.py
@@ -1,73 +1,49 @@
 board = [i for i in range(1, 10)]
-def my_board(board):
+
+
+def print_board(board):
     print("-" * 13)
     for i in range(3):
         print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
         print("-" * 13)
 
 
-def player(who):
-        X = int(input("Введите поле в диапазоне от 1 до 9 включительно "))
-        if str(board[X - 1]) not in "XY":
-            board[X - 1] = who
-            my_board(board)
+def move(who, player):
+    while True:
+        figure = int(input(f" {player}, введите поле в диапазоне от 1 до 9 включительно "))
+        if figure <= 9:
+            if str(board[figure - 1]) not in "XY":
+                board[figure - 1] = who
+                break
+        else:
+            print("Повторите попытку")
 
 
-
-def winner(board):
+def find_winner(board):
     winser = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
     for i in winser:
         if (board[i[0]]) == (board[i[1]]) == (board[i[2]]):
             return board[i[0]]
-    return False
+
 
 def main():
     amount = 0
-    win = False
-    while not win:
-        my_board(board)
+    while True:
+        print_board(board)
         if amount % 2 == 0:
-            player("X")
+            move("X", "Ходи X")
         else:
-            player("Y")
+            move("Y", "Ходи Y")
         if amount > 3:
-            gamer = winner(board)
+            gamer = find_winner(board)
             if gamer:
                 print(gamer, "win")
                 break
         amount += 1
         if amount == 9:
             print("Ничья")
-    my_board(board)
+    print_board(board)
 
 
 main()
 
-
-
-# class Tree():
-#     def __init__(self,a):
-#         self.a = a
-#         self.age = 0
-#         self.height = 0
-#     def info(self):
-#         return f"{self.a}, {self.age}, {self.height}"
-#
-#     def plus(self):
-#         self.age +=10
-#         self.height+=1
-# #
-# #
-# # class Fruit_Tree(Tree):
-# #     def amount_fruti(self):
-# #         s = self.age//2
-# #         return f"{s}"
-#
-#
-# c = Fruit_Tree("Tree")
-# c.plus()
-# c.plus()
-# c.info()
-# c.amount_fruti()
-# print(c.info())
-# print(c.amount_fruti())
